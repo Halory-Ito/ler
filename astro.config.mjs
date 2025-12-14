ce77b0f4c5a271070c5fb29fb0981f3039dd9ff5
Halory <2082004203@qq.com>
2025-12-14 15:28:53 +0800

    feat: add font

--- astro.config.mjs
+++ astro.config.mjs
@@ -77,7 +77,7 @@ export default defineConfig({
 				borderColor: "none",
 				codeFontSize: "0.875rem",
 				codeFontFamily:
-					"Consolas, 'JetBrains Mono Variable', ui-monospace, SFMono-Regular, Menlo, Monaco, 'Liberation Mono', 'Courier New', monospace",
+					"Consolas, '思源宋体 CN','JetBrains Mono Variable', ui-monospace, SFMono-Regular, Menlo, Monaco, 'Liberation Mono', 'Courier New', monospace",
 				codeLineHeight: "1.5rem",
 				frames: {
 					editorBackground: "var(--codeblock-bg)",
